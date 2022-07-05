52e2bfd5fe8df6478f276484aef4612859eba102
esnya <2088693+esnya@users.noreply.github.com>
2022-07-05 12:24:05 +0900

    :construction: wip: Improve collision

--- Scripts/Player/WalkingStation.cs
+++ Scripts/Player/WalkingStation.cs
@@ -31,9 +31,9 @@ namespace USS2
         public float stepHeight = 0.8f;
 
         /// <summary>
-        /// Width of player collider. Miminum hole width to pass.
+        /// Radius of player collider. Half width of miminum hole to pass.
         /// </summary>
-        public float playerWidth = 1.0f;
+        public float playerRadius = 0.2f;
 
         /// <summary>
         /// Height op player collider. Miminum hole height to pass.
@@ -50,6 +50,11 @@ namespace USS2
         /// Rotation speed.
         /// </summary>
         public float rotationSpeed = 200.0f;
+
+        /// <summary>
+        /// Delay frames to player fall off the vessel.
+        /// </summary>
+        public float exitCounter = 10;
 
         [UdonSynced(UdonSyncMode.Smooth)][FieldChangeCallback(nameof(SeatLocalPosition))] private Vector3 _seatLocalPosition;
         private Vector3 SeatLocalPosition
@@ -82,6 +87,7 @@ namespace USS2
         private Vector3 seatVelocity;
         private VRCStation station;
         private WalkingStationPool pool;
+        private int exitCount;
 
         private void Start()
         {
@@ -94,23 +100,46 @@ namespace USS2
         {
             if (!seated) return;
 
-            if (onGround)
-            {
-                seatVelocity = Vector3.zero;
-            }
-            else
-            {
-                if (Physics.Raycast(SeatPosition, Vector3.down, out hit, maxHeight, groundLayer, QueryTriggerInteraction.Ignore))
-                {
-                    SeatPosition += seatVelocity * Time.fixedDeltaTime;
-                    var deltaTime = Time.fixedDeltaTime;
-                    seatVelocity += Physics.gravity * deltaTime;
+            var deltaTime = Time.fixedDeltaTime;
 
-                    Move(seatVelocity);
+            if (onGround || seatVelocity.y <= 0)
+            {
+                var heightFromFloor = GetHeightFromFloor(SeatPosition);
+
+                if (heightFromFloor <= 0 || heightFromFloor <= Mathf.Clamp(-(seatVelocity.y + Physics.gravity.y) * deltaTime, 0, maxHeight))
+                {
+                    SeatPosition -= Vector3.up * heightFromFloor;
+                    onGround = true;
+                    exitCount = 0;
                 }
                 else
                 {
-                    _ExitStation();
+                    onGround = false;
+                    if (heightFromFloor <= maxHeight)
+                    {
+                        exitCount = 0;
+                    }
+                    else if (exitCount++ >= exitCounter)
+                    {
+                        _ExitStation();
+                    }
+                }
+            }
+
+            if (!Mathf.Approximately(seatVelocity.magnitude, 0.0f))
+            {
+                var prevSeatPosition = SeatPosition;
+                SeatPosition = GetNextPosition(SeatPosition, seatVelocity, deltaTime);
+
+                if (Mathf.Approximately(Vector3.Distance(SeatPosition, prevSeatPosition), 0.0f)) onGround = true;
+
+                if (onGround)
+                {
+                    seatVelocity = Vector3.zero;
+                }
+                else
+                {
+                    seatVelocity += Physics.gravity * deltaTime;
                 }
             }
         }
@@ -120,14 +149,10 @@ namespace USS2
             if (!seated) return;
 
             var moveInput = Vector3.right * Input.GetAxis("Horizontal") + Vector3.forward * Input.GetAxis("Vertical");
-            if (onGround && moveInput.magnitude > 0.0f) Move(moveInput * walkSpeed);
+            if (moveInput.sqrMagnitude > 0.0f) Move(moveInput * walkSpeed);
 
             var rotationInput = Input.GetAxisRaw("Oculus_CrossPlatform_SecondaryThumbstickHorizontal") + (Networking.LocalPlayer.IsUserInVR() ? 0.0f : Input.GetAxisRaw("Mouse X"));
             if (!Mathf.Approximately(rotationInput, 0)) Rotate(rotationInput * rotationSpeed);
-
-            // Debug.Log($"m: {moveInput}, r: {rotationInput} J1A4: {Input.GetAxis("Joy1 Axis 4"):F2}, MX: {Input.GetAxis("Mouse X"):F2}");
-
-            // if (Input.GetButton("Jump")) Jump();
         }
 
         public override void OnStationEntered(VRCPlayerApi player)
@@ -138,6 +163,7 @@ namespace USS2
                 onGround = true;
                 SeatLocalPosition = Vector3.zero;
                 SeatLocalRotation = 0.0f;
+                exitCount = 0;
             }
         }
 
@@ -146,7 +172,11 @@ namespace USS2
             if (player.isLocal)
             {
                 seated = false;
-                if (recoveryStation) recoveryStation.EnterStation();
+                if (recoveryStation)
+                {
+                    recoveryStation.transform.SetPositionAndRotation(SeatPosition, Quaternion.AngleAxis(SeatRotation, Vector3.up));
+                    recoveryStation.EnterStation();
+                }
                 if (pool) pool._ReturnStation(this);
 
                 SeatLocalPosition = Vector3.zero;
@@ -168,21 +198,8 @@ namespace USS2
         private void Move(Vector3 value)
         {
             if (!seated) return;
-
-            var deltaTime = Time.deltaTime;
-
-            var localPlayer = Networking.LocalPlayer;
-            var diffVector = Quaternion.FromToRotation(Vector3.forward, Vector3.ProjectOnPlane(localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation * Vector3.forward, Vector3.up)) * value * (GetPlayerSpeed() * deltaTime);
-            var nextPosition = SeatPosition + diffVector;
-
-            var collision = Physics.SphereCast(nextPosition + Vector3.up * (playerHeight - playerWidth * 0.5f), playerWidth * 0.5f, Vector3.down, out hit, playerHeight - playerWidth + stepHeight, groundLayer, QueryTriggerInteraction.Ignore);
-            var ydiff = collision ? hit.point.y - nextPosition.y : -maxHeight;
-            var canMove = !collision || ydiff < stepHeight;
-            onGround = ydiff > -stepHeight;
-
-            if (canMove) SeatPosition = nextPosition + Vector3.up * Mathf.Max(ydiff, 0.0f);
-
-            // Debug.Log($"Movew: onGround: {onGround}, canMove: {canMove}, ydiff: {ydiff:F2},  hit.distance: {hit.distance:F2}, hit.point: {hit.point}, hit.normal: {hit.normal}");
+            var xzVelocity = Vector3.ProjectOnPlane(Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation * value, Vector3.up).normalized * value.magnitude * GetPlayerSpeed();
+            seatVelocity = Vector3.Lerp(seatVelocity, xzVelocity + Vector3.Project(seatVelocity, Vector3.up), onGround ? 1.0f : Time.deltaTime);
         }
 
         private void Rotate(float speed)
@@ -211,7 +228,78 @@ namespace USS2
             station.ExitStation(Networking.LocalPlayer);
         }
 
+        private float GetHeightFromFloor(Vector3 position)
+        {
+            if (Physics.SphereCast(position + Vector3.up * stepHeight, playerRadius, Vector3.down, out hit, maxHeight + stepHeight, groundLayer, QueryTriggerInteraction.Ignore))
+            {
+                return position.y - hit.point.y;
+            }
+            return float.PositiveInfinity;
+        }
+
+        private Vector3 GetNextPosition(Vector3 position, Vector3 velocity, float deltaTime)
+        {
+            var maxDistance = velocity.magnitude * deltaTime;
+            if (Physics.CapsuleCast(position + Vector3.up * (stepHeight + playerRadius), position + Vector3.up * (playerHeight - playerRadius), playerRadius, velocity, out hit, maxDistance, groundLayer, QueryTriggerInteraction.Ignore))
+            {
+                var diff = Vector3.ClampMagnitude(Vector3.Project(hit.point - position, velocity), maxDistance);
+                return position + diff.normalized * Mathf.Clamp01(diff.magnitude - playerRadius);
+            }
+
+            return position + velocity * deltaTime;
+        }
+
 #if !COMPILER_UDONSHARP && UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            this.UpdateProxy();
+            if (!seated) return;
+
+            var position = SeatPosition;
+
+            Gizmos.color = onGround ? Color.green : Color.red;
+            DrawPlayerCapsule(position);
+            if (!onGround)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawRay(position, Vector3.down * Mathf.Max(GetHeightFromFloor(position), maxHeight));
+            }
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(position, seatVelocity);
+
+            var velocity = Vector3.ProjectOnPlane(Networking.LocalPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).rotation * Vector3.forward, Vector3.up).normalized * walkSpeed;
+            var deltaTime = 1.0f;
+            var maxDistance = velocity.magnitude * deltaTime;
+
+            var nextPosition = GetNextPosition(position, velocity, deltaTime);
+            Gizmos.color = Mathf.Approximately(Vector3.Distance(position, nextPosition), maxDistance) ? Color.blue : Color.red;
+            DrawPlayerCapsule(nextPosition);
+
+            var heightFromFloor = GetHeightFromFloor(nextPosition);
+            if (heightFromFloor <= stepHeight)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawRay(nextPosition, Vector3.down * heightFromFloor);
+            }
+            else if (heightFromFloor <= maxHeight)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawRay(nextPosition, Vector3.down * heightFromFloor);
+            }
+            else
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawRay(nextPosition, Vector3.down * maxHeight);
+            }
+        }
+
+        private void DrawPlayerCapsule(Vector3 position)
+        {
+            Gizmos.DrawWireSphere(position + Vector3.up * playerRadius, playerRadius);
+            Gizmos.DrawWireSphere(position + Vector3.up * (playerHeight - playerRadius), playerRadius);
+            Gizmos.DrawLine(position + Vector3.up * stepHeight, position + Vector3.up * playerHeight);
+        }
 #endif
     }
 }
