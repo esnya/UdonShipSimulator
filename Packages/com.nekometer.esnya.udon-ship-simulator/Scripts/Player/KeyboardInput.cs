e80da659b688c9b5861615c4f9f91e740e4eb492
esnya <2088693+esnya@users.noreply.github.com>
2022-07-20 13:54:56 +0900

    :construction: wip: Modifier key

--- Packages/com.nekometer.esnya.udon-ship-simulator/Scripts/Player/KeyboardInput.cs
+++ Packages/com.nekometer.esnya.udon-ship-simulator/Scripts/Player/KeyboardInput.cs
@@ -12,12 +12,18 @@ namespace USS2
         [ListView("Bindings")][Popup("GetKeyCodes")] public int[] keyCodes = { };
         [ListView("Bindings")] public UdonSharpBehaviour[] targets = { };
         [ListView("Bindings")][Popup("behaviour", "@targets")] public string[] eventNames = { };
+        [ListView("Bindings")] public bool[] controls = { };
+        [ListView("Bindings")] public bool[] shifts = { };
+        [ListView("Bindings")] public bool[] alts = { };
 
         private void Update()
         {
+            var ctrl = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+            var shift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            var alt = Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt);
             for (var i = 0; i < keyCodes.Length; i++)
             {
-                if (Input.GetKeyDown((KeyCode)keyCodes[i]))
+                if (Input.GetKeyDown((KeyCode)keyCodes[i]) && controls[i] == ctrl && shifts[i] == shift && alt == alts[i])
                 {
                     var target = targets[i];
                     if (target) targets[i].SendCustomEvent(eventNames[i]);
@@ -26,7 +32,8 @@ namespace USS2
         }
 
 #if !COMPILER_UDONSHARP && UNITY_EDITOR
-        public string[] GetKeyCodes() {
+        public string[] GetKeyCodes()
+        {
             return Enumerable.Range(0, (Enum.GetValues(typeof(KeyCode)) as int[]).Max()).Select(i => Enum.IsDefined(typeof(KeyCode), i) ? $"{(KeyCode)i}" : string.Empty).ToArray();
         }
 #endif
